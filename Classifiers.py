57e01f0209c0165a7586f45c380319822ed53141
Nicholas-Mitri <nicholas.g.mitri@gmail.com>
2017-07-22 18:48:39 +0300

    Add ensemble learners

--- Classifiers.py
+++ Classifiers.py
@@ -7,14 +7,17 @@ from sklearn.tree import DecisionTreeClassifier
 
 classifiers = {'dt': DecisionTreeClassifier(),
                'svm': SVC(),
+               'bagging': BaggingClassifier(),
                'mlp': MLPClassifier(),
-               
+               'ada': AdaBoostClassifier()}
 
 params = {'dt': OrderedDict([('criterion', ['entropy', 'gini']),
                              ('max_depth', [5, 10, 15]),
                              ('max_features', [0.2, 0.5, 0.8, 1]),
                              ('class_weight', [None, 'balanced'])]),
           'svm': OrderedDict([('C', [0.1, 0.5,1, 10]), ('gamma', [2**-5, 2**-3, 2**-2, 0.5, 1]), ('probability', [True])]),
+          'bagging': OrderedDict([('criterion', ['entropy']), ('max_depth', [10]), ('n_estimators', [20])]),
+          'ada': OrderedDict([('learning_rate', [1.0]), ('algorithm', ['SAMME.R']), ('n_estimators', [20])]),
           'mlp': OrderedDict([('activation', ['tanh', 'relu']),
                               ('alpha', [0.001, 0.1, 1, 10]),
                               ('hidden_layer_sizes', [(10, 10), (10, 20, 10), (50, 50, 50)]),
